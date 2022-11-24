fd92fa39a285c02ae8316b78c1f805ec3c0fdf70
VAISHALI <VAISHALI@DESKTOP-VVTS57G>
2022-11-24 19:00:37 +0530

    hi activity2

---
+++ Activity2/src/com/question3/CustomerMain.java
@@ -0,0 +1,21 @@
+package com.question3;
+
+
+
+public class CustomerMain {
+	
+
+	public static void main(String[] args) {
+		
+		Customer c=new Customer();
+		
+		c.createAccount();
+		c.setAmount();
+		c.getAmount();
+		c.withDrawAmount();
+		
+		
+
+	}
+
+}
