54bbb79a0ec6393f3761b32c53d0a98b57eb000a
VAISHALI <VAISHALI@DESKTOP-VVTS57G>
2022-12-23 10:48:33 +0530

    Activity6

--- Activity6/src/com/Question3/Fruit.java
+++ Activity6/src/com/Question3/Fruit.java
@@ -1,27 +1,40 @@
 package com.Question3;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Fruit {
 
 	public static void main(String[] args) {
-		ArrayList<String> FruitBucket1=new ArrayList<>(); //FruitBucket1
-		FruitBucket1.add("Mango");
-		FruitBucket1.add("Apple");
-		FruitBucket1.add("Orange");
-		System.out.println(FruitBucket1);
+		ArrayList<String> FruitBasket1=new ArrayList<>(); //FruitBasket1
+		FruitBasket1.add("Mango");
+		FruitBasket1.add("Apple");
+		FruitBasket1.add("Orange");
+		System.out.println("This is FruitBasket1"+FruitBasket1);
 		System.out.println("-------------------------------------------");
-		ArrayList<String> FruitBucket2= new ArrayList<>(); //FruitBucket2
-		FruitBucket2.add("WaterMelon");
-		FruitBucket2.add("Grapes");
-		FruitBucket2.add("Cherry");
-		System.out.println(FruitBucket2);
-		FruitBucket1.addAll(FruitBucket2); //Adding FruitBucket1 and FruitBucket2
-		System.out.println(FruitBucket1);
-		System.out.println("Whether mango is in the FruitBucket1?"+FruitBucket1.contains("Mango")); //checking fruit present or not
-		System.out.println("Whether FruitBucket1 is empty or not"+FruitBucket1.isEmpty()); //checking FruitBucket1 is empty or not
-		FruitBucket1.remove("Apple"); //removing one fruit from FruitBucket1
-		System.out.println(FruitBucket1);
+		ArrayList<String> FruitBasket2= new ArrayList<>(); //FruitBascket2
+		FruitBasket2.add("WaterMelon");
+		FruitBasket2.add("Grapes");
+		FruitBasket2.add("Cherry");
+		System.out.println("This is Fruitbasket2 "+FruitBasket2);
+		System.out.println("-------------------------------------------");
+		Collections.sort(FruitBasket1);
+		System.out.println("after sorting in ascending order:"+FruitBasket1); //sorting the list 1st in ascending order
+		System.out.println("-------------------------------------------");
+		Collections.sort(FruitBasket2,Collections.reverseOrder());
+		System.out.println("after sorting in descending order"+FruitBasket2); //sorting the list 2nd in descending order
+		System.out.println("-------------------------------------------");
+		FruitBasket1.addAll(FruitBasket2); //Adding FruitBucket1 and FruitBascket2
+		System.out.println("after adding the elements from FruitBasket2 to FruitBasket1"+FruitBasket1);
+		System.out.println("-------------------------------------------");
+		System.out.println("Whether mango is in the FruitBucket1?"+FruitBasket1.contains("Mango"));
+		System.out.println("-------------------------------------------");//checking fruit present or not
+		FruitBasket1.clear();
+		System.out.println("Whether FruitBucket1 is empty or not"+FruitBasket1.isEmpty()); //checking FruitBucket1 is empty or not
+        System.out.println("before removing the element from the specific position"+FruitBasket2);
+        System.out.println("-------------------------------------------");
+		FruitBasket2.remove("Grapes"); //removing one fruit from FruitBucket1
+		System.out.println("after removing the element from the specific position:"+FruitBasket2);
 		
 
 	}
